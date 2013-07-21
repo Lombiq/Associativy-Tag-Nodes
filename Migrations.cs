d66dc662ab56bae4436289caf72a05d2a0a3f337
Piedone <piedone@pyrocenter.hu>
2013-07-21 15:35:04 +0200

    Initial commit with migrations taken from Lombiq projects

---
+++ Migrations.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Orchard.ContentManagement.MetaData;
+using Orchard.Core.Contents.Extensions;
+using Orchard.Data.Migration;
+using Orchard.Environment.Extensions;
+
+namespace Lombiq.Associativy.TagNodes
+{
+    public class Migrations : DataMigrationImpl
+    {
+        public int Create()
+        {
+            ContentDefinitionManager.AlterTypeDefinition("TagNode",
+                    cfg => cfg
+                        .WithPart("CommonPart", part => part
+                            .WithSetting("DateEditorSettings.ShowDateEditor", "false")
+                            .WithSetting("OwnerEditorSettings.ShowOwnerEditor", "false"))
+                        .WithPart("AssociativyNodeManagementPart")
+                        .WithPart("AssociativyNodeTitleLabelPart")
+                        .WithPart("ImplicitlyCreatableAssociativyNodePart")
+                        .Creatable()
+                );
+
+
+            return 1;
+        }
+    }
+}
